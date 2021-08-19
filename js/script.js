47a9ec8e74c3b731bae41098c8a93121e6e8347b
ilucradis <mati01herrera@gmail.com>
2021-08-19 18:21:54 -0300

    entrega final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const hamburger = document.getElementsByClassName('hamburger')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const nav1 = document.getElementsByClassName("navbar-a")[1]
+const nav2 = document.getElementsByClassName("navbar-a")[2]
+const nav3 = document.getElementsByClassName("navbar-a")[3]
 
 hamburger.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+nav1.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+nav2.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+nav3.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
