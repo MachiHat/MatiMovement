7d4607a317dc05d6fac36b7fac557d9fc46a70af
ilucradis <mati01herrera@gmail.com>
2021-10-15 12:41:59 -0300

    javascript last update: shop, filter, and buttons

--- js/filter.js
+++ js/filter.js
@@ -45,58 +45,68 @@ $(".list-btn").click(function(){
 
 // VIDEO BUTTONS
 
+/* --- WIP --- WIP --- WIP ---
+function buttonOnVideo() {
+    var videoButtons = document.getElementbyClassName("excercise-item")[i];
+    for (var i = 0; i < videoButtons.length; i++) {
+        var videoButton = videoButtons[i];
+        videoButton.addEventListener("click", fa.classList.toggle("onVideo"));
+    }
+}
+*/
+
 $("#video-item-1 button").click(function(){
     $("#video-1").addClass("onVideo");
-})
+});
 
 $("#video-item-2 button").click(function(){
     $("#video-2").addClass("onVideo");
-})
+});
 
 $("#video-item-3 button").click(function(){
     $("#video-3").addClass("onVideo");
-})
+});
 
 $("#video-item-4 button").click(function(){
     $("#video-4").addClass("onVideo");
-})
+});
 
 $("#video-item-5 button").click(function(){
     $("#video-5").addClass("onVideo");
-})
+});
 
 $("#video-item-6 button").click(function(){
     $("#video-6").addClass("onVideo");
-})
+});
 
 $("#video-item-7 button").click(function(){
     $("#video-7").addClass("onVideo");
-})
+});
 
 $("#video-item-8 button").click(function(){
     $("#video-8").addClass("onVideo");
-})
+});
 
 $("#video-item-9 button").click(function(){
     $("#video-9").addClass("onVideo");
-})
+});
 
 $("#video-item-10 button").click(function(){
     $("#video-10").addClass("onVideo");
-})
+});
 
 $("#video-item-11 button").click(function(){
     $("#video-11").addClass("onVideo");
-})
+});
 
 $("#video-item-12 button").click(function(){
     $("#video-12").addClass("onVideo");
-})
+});
 
 $("#video-item-13 button").click(function(){
     $("#video-13").addClass("onVideo");
-})
+});
 
 $("#video-item-14 button").click(function(){
     $("#video-14").addClass("onVideo");
-})
+});
--- js/script.js
+++ js/script.js
@@ -7,70 +7,23 @@ $(document).ready(function(){
 // NAVBAR RESPONSIVE BUTTON
 
 //var defs
-const hamburger = document.getElementsByClassName('hamburger')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-const nav1 = document.getElementsByClassName('navbar-a')[1]
-const nav2 = document.getElementsByClassName('navbar-a')[2]
-const nav3 = document.getElementsByClassName('navbar-a')[3]
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const nav1 = document.getElementsByClassName('navbar-a')[1];
+const nav2 = document.getElementsByClassName('navbar-a')[2];
+const nav3 = document.getElementsByClassName('navbar-a')[3];
 
 //events
 hamburger.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
 nav1.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
 nav2.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
 nav3.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
  
-
-// SIGNUP AND LOGIN FORMS
-
-var newUsername = $('suname');
-var newMail = $('sumail');
-var newPass = $('supw');
-
-function newAcc (username, mail, pass){
-  this.username = newUsername;
-  this.mail = newMail;
-  this.password = newPass;
-}
-
-
-
-/*
-
-//var defs
-
-var suname = $(".suname");
-var sumail = $("#sumail");
-var supw = $("#supw");
-
-//data storing function
-function store() {
-    localStorage.setItem('suname', suname.value);
-    localStorage.setItem('sumail', sumail.value);
-    localStorage.setItem('supw', supw.value);
-}
-
-//login data check
-function check() {
-
-  var storedName = localStorage.getItem('suname');
-  var storedPw = localStorage.getItem('supw');
-
-  var liname = $("#liname");
-  var lipw = $("#lipw");
-
-  if(liname.value == storedName && lipw.value == storedPw) {
-    alert('You are loged in.');
-  }else {
-    alert('ERROR.');
-  }
-}
-
-*/
